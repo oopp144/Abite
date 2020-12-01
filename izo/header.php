257eeb64da0fed4228507534773319776bbc9e4d
Jonny <oopp144@gmail.com>
2020-12-01 13:44:25 +1100

    add IZO

---
+++ izo/header.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Izo
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'izo' ); ?></a>
+
+	<?php do_action( 'izo_header_before' ); ?>
+
+	<?php do_action( 'izo_header' ); ?>
+
+	<?php do_action( 'izo_header_after' ); ?>
+
+	<?php do_action( 'izo_main_container_start' ); ?>
